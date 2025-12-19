42b8bd7158687030f8732b995503251e4c256513
phuongluuho <65580209+phuongluuho@users.noreply.github.com>
2025-12-19 16:55:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Configure Pug as the template engine and the directory for the views.
 app.set('view engine', 'pug');
